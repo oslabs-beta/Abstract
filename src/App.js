09f0435c27f550ae4c5ee6abd70c008309571023
omar brown <omarbrown@omars-MacBook-Air.local>
2021-09-13 14:26:59 -0700

    Log in page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <div>
+        <h1> Welcome to Abstract! </h1>
+        <h1> Please log in via Github. </h1>
+        </div>
+        <button id="logIn"> <img src="https://pngimg.com/uploads/github/github_PNG80.png" alt="githubLogo" id="githubLogo"/> Log in with Github </button>
+        <h3> Don't have a Github account? </h3>
+
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        </p> */}
+        <button
+          id="SignUpBtn"
+          
+          // target="_blank"
+          // rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Sign up
+        </button>
       </header>
     </div>
   );
