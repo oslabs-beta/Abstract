7296747340817cffa4518ff4843b635484eaba2e
omar brown <omarbrown@omars-MacBook-Air.local>
2021-09-14 10:48:50 -0700

    cleaned up src

--- src/App.js
+++ src/App.js
@@ -5,23 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <div>
         <h1> Welcome to Abstract! </h1>
         <h1> Please log in via Github. </h1>
         </div>
         <button id="logIn"> <img src="https://pngimg.com/uploads/github/github_PNG80.png" alt="githubLogo" id="githubLogo"/> Log in with Github </button>
         <h3> Don't have a Github account? </h3>
-
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        <button
-          id="SignUpBtn"
-          
-          // target="_blank"
-          // rel="noopener noreferrer"
-        >
+        <button id="SignUpBtn" >
           Sign up
         </button>
       </header>
