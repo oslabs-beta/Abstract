df1c612a7314b84293a6c4a82da45e32fdede943
Raymond <you@example.com>
2021-09-17 22:43:53 -0700

    added settings for navbar and nav components in bootstrap Co-authored-by: Brian Cheng chengbrian9@gmail.com

--- src/components/BootstrapLibrary.jsx
+++ src/components/BootstrapLibrary.jsx
@@ -2,7 +2,7 @@ import BootstrapComponent from './BootstrapComponent';
 
 function BootstrapLibrary ( {library} ) {
   let BootstrapComponents = []
-  let name = ['button', 'form', 'navbar', 'list']
+  let name = ['button', 'form', 'navbar', 'nav', 'list']
   if (library === 'BootstrapLibrary') {
     for (let x = 0; x < name.length; x++){
       BootstrapComponents.push(<BootstrapComponent key={x} name={name[x]}/>)
--- src/components/ComSettings.jsx
+++ src/components/ComSettings.jsx
@@ -116,10 +116,10 @@ function ComSettings(props) {
       )
     case 'form':
       function renderFormSettings() {
-        if (methods === 'form') {
+        if (methods === 'Form.Control') {
           return (
           <>
-            <h1>Form Component Settings</h1> 
+            <h2>Form.Control Component Settings</h2>
             <Form>
             <Form.Group>
               <Form.Label>Parent Component</Form.Label>
@@ -127,98 +127,268 @@ function ComSettings(props) {
                 <option>1</option>
               </Form.Select>
               <Form.Label>Class Name</Form.Label>
-              <Form.Control type='class_name' placeholder='Enter a class name (optional)'/>
+              <Form.Control type='class_name' placeholder='class name (optional)'/>
               <Form.Label>Component ID</Form.Label>
-              <Form.Control type='component_id'></Form.Control>
-              <Form.Label>Font Size</Form.Label>
-              <Form.Control type='font_size'></Form.Control>
+              <Form.Control type='component_id' placeholder='Component identifier'></Form.Control>
+              <Form.Label>Type</Form.Label>
+              <Form.Control type='type' placeholder='Type'/>
+              <Form.Label>Value</Form.Label>
+              <Form.Control type='value' placeholder='Value'/>
+              <Form.Label>Placeholder Text</Form.Label>
+              <Form.Control type='placeholder' placeholder='Placeholder'/>
+              <Form.Label>Size</Form.Label>
+              <Form.Control type='size' placeholder='size of form'/>
+              <Form.Check type="checkbox" label='Required'/>
             </Form.Group>
             </Form>
           <AddButton></AddButton>
           </>
           )
-        } else if (methods === 'form-group') {
+        } else if (methods === 'Form.Group') {
           return (
             <>
-              <h1>Form.Group Component Settings</h1>
-              <Form>
-                <Form.Group>
-                <Form.Label>Parent Component</Form.Label>
-                <Form.Select>
-                  <option>1</option>
-                </Form.Select>
-                <Form.Label>Class Name</Form.Label>
-                <Form.Control type='class_name' placeholder='(optional)'/>
-                <Form.Label>Component ID</Form.Label>
-                <Form.Control type='component_id' placeholder='Enter an ID'/>
-                <Form.Label>Font Size</Form.Label>
-                <Form.Control type='font_size' placeholder='Enter a number'/>
-                <Form.Label>Placeholder Text</Form.Label>
-                <Form.Control type='placeholder' placeholder='Placeholder'/>
-                <Form.Label>Rows</Form.Label>
-                <Form.Control type='rows' placeholder='# of Rows'/>
-                <Form.Label>Size</Form.Label>
-                <Form.Control type='size' placeholder='size of form'/>
-                <Form.Label>Type</Form.Label>
-                <Form.Control type='type' placeholder='Type'/>
-                <Form.Label>Default Value</Form.Label>
-                <Form.Control type='default_value' placeholder='Default Value'/>
-                <Form.Label>Value</Form.Label>
-                <Form.Control type='value' placeholder='Value'/>
-                <Form.Label>Title</Form.Label>
-                <Form.Control type='title' placeholder='Title'/>
-                <Form.Label>Label</Form.Label>
-                <Form.Control type='label' placeholder='Label'/>
-                </Form.Group>
-              </Form>
-              <AddButton></AddButton>
-            </>
-              )
-          } else if (methods === 'form-label') {
-            return (
-              <>
-                <Form>
-                  <Form.Group>
-                    <Form.Label>Parent Component</Form.Label>
-                    <Form.Select>
-                      <option>1</option>
-                    </Form.Select>
-                    <Form.Label>Font Size</Form.Label>
-                    <Form.Control type='font_size'></Form.Control>
-                  </Form.Group>
-                </Form>
-                <AddButton></AddButton>
-              </>
-            );
-          } else return (
-            <>
+              <h2>Form.Group Component Settings</h2>
               <Form>
                 <Form.Group>
                   <Form.Label>Parent Component</Form.Label>
                   <Form.Select>
                     <option>1</option>
                   </Form.Select>
-                  <Form.Label>Font Size</Form.Label>
-                  <Form.Control type='font_size'></Form.Control>
+                  <Form.Label>Class Name</Form.Label>
+                  <Form.Control type='class_name' placeholder='(optional)'/>
+                  <Form.Label>Component ID</Form.Label>
+                  <Form.Control type='component_id' placeholder='Enter an ID'/>
+                </Form.Group>
+              </Form>
+              <AddButton></AddButton>
+            </>
+              )
+          } else if (methods === 'Form.Label') {
+            return (
+              <>
+                <h2>Form.Label Component Settings</h2>
+                <Form>
+                  <Form.Group>
+                    <Form.Label>Parent Component</Form.Label>
+                    <Form.Select>
+                      <option>1</option>
+                    </Form.Select>
+                    <Form.Label>Class Name</Form.Label>
+                    <Form.Control type='class_name' placeholder='(optional)'/>
+                    <Form.Label>Component ID</Form.Label>
+                    <Form.Control type='component_id' placeholder='Enter an ID'/>
+                  </Form.Group>
+                </Form>
+                <AddButton></AddButton>
+              </>
+            );
+          } else if (methods === 'Form') {
+              return (
+            <>
+              <h2>Form Component Settings</h2>
+              <Form>
+                <Form.Group>
+                  <Form.Label>Parent Component</Form.Label>
+                  <Form.Select>
+                    <option>1</option>
+                  </Form.Select>
                 </Form.Group>
               </Form>
               <AddButton></AddButton>
             </>
             )
+          }
         } return (
         <>
-          <h1>Form Component Settings</h1>
-            <DropdownButton id="dropdown-item-button" title="Form">
-            <Dropdown.Item as="button" onClick={() => setMethods('form')}>Form</Dropdown.Item>
-            <Dropdown.Item as="button" onClick={() => setMethods('form-group')}>Form.Group</Dropdown.Item>
-            <Dropdown.Item as="button" onClick={() => setMethods('form-label')}>Form.Label</Dropdown.Item>
-            <Dropdown.Item as="button" onClick={() => setMethods('form-control')}>Form.Control</Dropdown.Item>
-            </DropdownButton>
-            { renderFormSettings() }
+          <DropdownButton id="dropdown-item-button" title='Select'>
+            <Dropdown.Item as="button" onClick={() => setMethods('Form')}>Form</Dropdown.Item>
+            <Dropdown.Item as="button" onClick={() => setMethods('Form.Control')}>Form.Control</Dropdown.Item>
+            <Dropdown.Item as="button" onClick={() => setMethods('Form.Group')}>Form.Group</Dropdown.Item>
+            <Dropdown.Item as="button" onClick={() => setMethods('Form.Label')}>Form.Label</Dropdown.Item>
+          </DropdownButton>
+          { renderFormSettings() }
         </>
       )
-      default: 
-      return (
+    
+      case 'navbar':
+        function renderNavbarSettings() {
+          if (methods === 'Navbar') {
+            return (
+              <>
+                <h2>Navbar Component Settings</h2>
+                <Form>
+                  <Form.Group>
+                    <Form.Label>Parent Component</Form.Label>
+                    <Form.Select>
+                      <option>1</option>
+                    </Form.Select>
+                    <Form.Label>Class Name</Form.Label>
+                    <Form.Control type='class_name' placeholder='class name (optional)'/>
+                    <Form.Label>Component ID</Form.Label>
+                    <Form.Control type='component_id' placeholder='Component identifier'></Form.Control>
+                    <Form.Label>Fixed</Form.Label>
+                    <Form.Select>
+                      <option>top</option>
+                      <option>bottom</option>
+                    </Form.Select>
+                    <Form.Check type='sticky' label='Sticky'/>
+                    <Form.Label>Variant</Form.Label>
+                    <Form.Select>
+                      <option>light</option>
+                      <option>dark</option>
+                    </Form.Select>
+                    <Form.Label>href</Form.Label>
+                    <Form.Control type=''placeholder='#home'/>
+                    <Form.Label>Background Color</Form.Label>
+                    <Form.Select>
+                      <option>light</option>
+                      <option>dark</option>
+                    </Form.Select>
+                    <Form.Label>Expand</Form.Label>
+                    <Form.Select>
+                      <option>sm</option>
+                      <option>md</option> 
+                      <option>lg</option> 
+                      <option>xl</option> 
+                      <option>xxl</option>  
+                    </Form.Select>
+                  </Form.Group>
+                </Form>
+                <AddButton></AddButton>
+              </>
+            )
+          } else if (methods === 'Navbar.Brand') {
+            return (
+              <>
+                <h2>Navbar.Brand Component Settings</h2>
+                <Form>
+                  <Form.Group>
+                    <Form.Label>Parent Component</Form.Label>
+                    <Form.Select>
+                      <option>1</option>
+                    </Form.Select>
+                    <Form.Label>href</Form.Label>
+                    <Form.Control type='href' placeholder='#home'/>
+                  </Form.Group>
+              </Form>
+              <AddButton></AddButton>
+              </>
+            )
+          } else if (methods === 'Navbar.Toggle') {
+            return (
+              <>
+                <h2>Navbar.Toggle Component Settings</h2>
+                <Form>
+                  <Form.Group>
+                    <Form.Label>Parent Component</Form.Label>
+                    <Form.Select>
+                      <option>1</option>
+                    </Form.Select>
+                    <Form.Label>Aria Controls</Form.Label>
+                    <Form.Select>
+                      <option>basic</option>
+                      <option>responsive</option>
+                    </Form.Select>
+                  </Form.Group>
+              </Form>
+              <AddButton></AddButton>
+              </>
+            )
+          } else if (methods === 'Navbar.Collapse') {
+            return (
+              <>
+                <h2>Navbar.Collapse Component Settings</h2>
+                <Form>
+                  <Form.Group>
+                    <Form.Label>Parent Component</Form.Label>
+                    <Form.Select>
+                      <option>1</option>
+                    </Form.Select>
+                    <Form.Label>Class Name</Form.Label>
+                    <Form.Control type='class_name' placeholder='class name (optional)'></Form.Control>
+                  </Form.Group>
+              </Form>
+              <AddButton></AddButton>
+              </>
+            )
+          }
+        } return (
+          <>
+            <DropdownButton id="dropdown-item-button" title='Select'>
+              <Dropdown.Item as="button" onClick={() => setMethods('Navbar')}>Navbar</Dropdown.Item>
+              <Dropdown.Item as="button" onClick={() => setMethods('Navbar.Brand')}>Navbar.Brand</Dropdown.Item>
+              <Dropdown.Item as="button" onClick={() => setMethods('Navbar.Toggle')}>Navbar.Toggle</Dropdown.Item>
+              <Dropdown.Item as="button" onClick={() => setMethods('Navbar.Collapse')}>Navbar.Collapse</Dropdown.Item>
+            </DropdownButton>
+            { renderNavbarSettings() }
+          </>
+        )
+      case 'nav': 
+        function renderNavSettings() {
+          if (methods === 'Nav') {
+            return (
+              <>
+                <h2>Navbar.Collapse Component Settings</h2>
+                <Form>
+                  <Form.Group>
+                    <Form.Label>Parent Component</Form.Label>
+                    <Form.Select>
+                      <option>1</option>
+                    </Form.Select>
+                    <Form.Label>Class Name</Form.Label>
+                    <Form.Control type='class_name' placeholder='class name (optional)'></Form.Control>
+                  </Form.Group>
+              </Form>
+              <AddButton></AddButton>
+            </>
+            )
+          } else if (methods === 'Nav.Link') {
+            return (
+             <>
+                <h2>Navbar.Collapse Component Settings</h2>
+                <Form>
+                  <Form.Group>
+                    <Form.Label>Parent Component</Form.Label>
+                    <Form.Select>
+                      <option>1</option>
+                    </Form.Select>
+                    <Form.Label>Class Name</Form.Label>
+                    <Form.Control type='class_name' placeholder='class name (optional)'></Form.Control>
+                  </Form.Group>
+              </Form>
+              <AddButton></AddButton>
+            </>
+            )
+          } else if (methods === 'Nav.DropDown') {
+            return (
+              <>
+                <h2>Navbar.Collapse Component Settings</h2>
+                <Form>
+                  <Form.Group>
+                    <Form.Label>Parent Component</Form.Label>
+                    <Form.Select>
+                      <option>1</option>
+                    </Form.Select>
+                    <Form.Label>Class Name</Form.Label>
+                    <Form.Control type='class_name' placeholder='class name (optional)'></Form.Control>
+                  </Form.Group>
+              </Form>
+              <AddButton></AddButton>
+            </>
+            )
+          }
+        }  return (
+          <>
+            <DropdownButton id="dropdown-item-button" title='Select'>
+              <Dropdown.Item as="button" onClick={() => setMethods('Nav')}>Nav</Dropdown.Item>
+              <Dropdown.Item as="button" onClick={() => setMethods('Nav.Link')}>Nav.Link</Dropdown.Item>
+              <Dropdown.Item as="button" onClick={() => setMethods('Nav.DropDown')}>Navbar.Toggle</Dropdown.Item>
+            </DropdownButton>
+            { renderNavSettings() }
+          </>
+        )
+        default: 
+        return (
         <>
         </>
       )
@@ -226,26 +396,16 @@ function ComSettings(props) {
 }
 
 {/* BootstrapElements
-    button
-      parent component
-      variant
-      size
-      class
-      id
-    form
-      method*
-      parent component
-      class
-      id
-      placeholder
-      rows
-      size
-      type
-      default value
-      value
-      title
-      label 
     navbar
+    - Navbar.Brand
+    - Navbar.toggle
+    - navbar.collapse
+
+    Nav
+      - nav.link
+      - NavDropDown
+      - NavDropdown.Item
+
       method*
       parent component
       class
