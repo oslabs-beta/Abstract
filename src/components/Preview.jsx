bd0c3d6a36a4470bb2024ec74d60f147807b2488
Brian Cheng <briancheng@Brians-MacBook-Pro.local>
2021-09-20 10:51:00 -0700

    Added code preview

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,7 +1,39 @@
-function Preview() {
+import { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+
+const mapStateToProps = (state) => ({
+  canvasSize: state.main.canvasSize
+});
+
+function Preview(props) {
+  //need a helper function to add a \ before template literals
+  const string = `function HelloWorld(){
+  p { color: red; } 
+  \`code { background-color: #eee; }
+  }
+  <div>
+    hi world 
+  </div>\``
+  const [canvasStyle, setCanvasStyle] = useState({ width: '683px', height: '512px' });
+
+  useEffect(() => {
+    if (props.canvasSize === 'iPad Pro') {
+      setCanvasStyle({ width: '683px', height: '512px' })
+    } else if (props.canvasSize === 'iPhone X') {
+      setCanvasStyle({ width: '406px', height: '187.5px' })
+    }
+  }, [props.canvasSize])
   return (
-    <p>Code Preview</p>
+    <div id="preview_backdrop" style={ { ...canvasStyle, backgroundColor: "green" } }>
+      <h3>hello it me, preview backdrop</h3>
+      <pre>
+        <code>
+          {string}
+        </code>
+      </pre>
+      
+    </div>
   )
 };
 
-export default Preview;
+export default connect(mapStateToProps, null)(Preview);
