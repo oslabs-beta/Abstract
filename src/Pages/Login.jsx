c6386a4a000ddffa1211870680c553a2b2aabdc6
Jonnie Oak <jonathan.oak28@gmail.com>
2021-10-02 18:04:26 -0700

    fix: revised GitHub OAuth redirect uri  in Login page from localhost:5000 to https://abstractreact.herokuapp.com/

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -18,7 +18,7 @@ function Login() {
             <h1 id="welcomeMessage"> Welcome to Abstract! <br/> Please log in via Github. </h1>
             <br/>
               <div>
-               <a href='https://github.com/login/oauth/authorize?client_id=46fc52b044a6de2f4a82&scope=read:user%20repo&redirect_uri=http://localhost:5000/oauth'>
+               <a href='https://github.com/login/oauth/authorize?client_id=46fc52b044a6de2f4a82&scope=read:user%20repo&redirect_uri=https://abstractreact.herokuapp.com/oauth'>
                   <button id="frontPageLogInButton">
                     <Icon icon="cib:github" id="gitHubLogo"/> Login with Github 
                   </button> <br/>
